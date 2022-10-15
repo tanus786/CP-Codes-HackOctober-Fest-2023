4895c2df8cbffd310ebcbf1f11ced76c6e587fd1
Bharti Meena <60185429+bhartimeena@users.noreply.github.com>
2022-10-16 03:34:44 +0530

    HacktoberFest2022

---
+++ A_Domino_piling.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,m;
+    cin>>n>>m;
+    // int ans = floor(n*m*0.5);
+    int ans = n*m/2;
+    cout<<ans<<endl;
+    return(0);
+
+}
