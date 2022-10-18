a91cea344e9a1079b7a5a7c23aa7b0916f0f9110
Sidharth P <58136648+Sidharth-P7@users.noreply.github.com>
2022-10-18 19:23:36 +0530

    Create Day of week.c

---
+++ Day of week.c
@@ -0,0 +1,23 @@
+// inputs switch statements inside of mainfn witch (day) {
+  case 1:
+    printf("Monday");
+    break;
+  case 2:
+    printf("Tuesday");
+    break;
+  case 3:
+    printf("Wednesday");
+    break;
+  case 4:
+    printf("Thursday");
+    break;
+  case 5:
+    printf("Friday");
+    break;
+  case 6:
+    printf("Saturday");
+    break;
+  case 7:
+    printf("Sunday");
+    break;
+}
