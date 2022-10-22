fe183ac1c461666d464f457c34c6e75756a50b87
KUNNAL BANSAL <79396203+kunna67@users.noreply.github.com>
2022-10-22 22:57:26 +0530

    Create 0025-reverse-nodes-in-k-group.cpp

---
+++ 0025-reverse-nodes-in-k-group.cpp
@@ -0,0 +1,34 @@
+class Solution 
+{
+public:
+    
+    ListNode* reverse(ListNode* first, ListNode* last)
+    {
+        ListNode* prev = last;
+        
+        while ( first != last )
+        {
+            auto tmp = first->next;
+            first->next = prev;
+            prev = first;
+            first = tmp;
+        }
+        
+        return prev;
+    }
+    
+    ListNode* reverseKGroup(ListNode* head, int k) 
+    {
+        auto node=head;
+        for (int i=0; i < k; ++i)
+        {
+            if ( ! node  )
+                return head; // nothing to do list too sort
+            node = node->next;
+        }
+
+        auto new_head = reverse( head, node);
+        head->next = reverseKGroup( node, k);
+        return new_head;
+    }
+};
