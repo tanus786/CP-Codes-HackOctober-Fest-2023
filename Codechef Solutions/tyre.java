d12b4164af7a844043293ce2a53779e19731b8e4
Keshav Banka <71920541+Keshav76@users.noreply.github.com>
2022-10-23 17:01:55 +0530

    Create tyre.java

---
+++ Codechef Solutions/tyre.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int i = 0;i<T;i++){
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+	
+	    System.out.println(N*2+M*4);
+		
+	
+		
+		}
+	}
+}
