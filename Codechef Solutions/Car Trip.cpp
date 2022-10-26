c5a6c8f1f5153c22be5d187feb22c9864a7d5272
morningstar201 <116706996+morningstar201@users.noreply.github.com>
2022-10-26 12:04:10 +0530

    Create Car Trip.cpp

---
+++ Codechef Solutions/Car Trip.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	int tc;
+	cin>>tc;
+	while(tc--){
+		int X;
+		cin>>X;
+		if(X <= 300){
+			cout<<"3000"<<"\n";
+		}else{
+			cout<<X * 10 <<"\n";
+		}
+	}
+	return 0;
+}
+       
