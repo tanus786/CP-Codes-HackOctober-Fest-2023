d6bc2a1f3845ecec7ec60d13f6c0611b3aedf591
lordde2134 <116729623+lordde2134@users.noreply.github.com>
2022-10-26 18:09:22 +0530

    Create Life The Universe and Everything.cpp

---
+++ Codechef Solutions/Life The Universe and Everything.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // your code goes here
+        int n;
+    scanf("%d",&n);
+        if(n!=42 && n<100)
+        {
+           printf("%d\n",n);
+           return main();
+        }
+    return 0;
+}
