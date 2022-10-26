1751d72e32ac71b5749593e3527ba036da92a9dc
viratden <116731088+viratden@users.noreply.github.com>
2022-10-26 18:27:50 +0530

    Create Chef and Difficult Contests.cpp

---
+++ Codechef Solutions/Chef and Difficult Contests.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define For(i,a,b) for(int i=a;i<b;i++)
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b;
+        cin>>a>>b;
+        cout<<"YES"<<endl;
+    }
+    return 0;
+}
