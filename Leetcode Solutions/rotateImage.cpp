334616f4eaf7c3ccd7e7f10b173a769a7bec419b
Saitama <116707931+cape-baldy@users.noreply.github.com>
2022-10-26 12:39:46 +0530

    Create rotateImage.cpp

---
+++ Leetcode Solutions/rotateImage.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+void rotate(vector<vector<int>>& matrix) {
+      int n = matrix.size();
+        int a = 0;
+        int b = n-1;
+        while(a<b){
+            for(int i=0;i<(b-a);++i){
+                swap(matrix[a][a+i], matrix[a+i][b]);
+                swap(matrix[a][a+i], matrix[b][b-i]);
+                swap(matrix[a][a+i], matrix[b-i][a]);
+            }
+            ++a;
+            --b;
+        }
+}
+};
+
+
+//https://leetcode.com/problems/rotate-image/
