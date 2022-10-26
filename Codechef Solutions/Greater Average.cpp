34b089276ac6d59a502a5c7f8376676eaa9a4e12
morningstar201 <116706996+morningstar201@users.noreply.github.com>
2022-10-26 12:01:29 +0530

    Create Greater Average.cpp

---
+++ Codechef Solutions/Greater Average.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    float a,b,c;
+	    cin>>a>>b>>c;
+	    if((a+b)/2>c)
+	    {
+	        cout<<"YES"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
+       
