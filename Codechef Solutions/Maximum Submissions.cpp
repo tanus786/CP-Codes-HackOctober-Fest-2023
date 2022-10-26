7914a214cbb54cafa21d9495d3d90cfc0c4ef5f4
arsenic011d <116727722+arsenic011d@users.noreply.github.com>
2022-10-26 17:43:07 +0530

    Create Maximum Submissions.cpp

---
+++ Codechef Solutions/Maximum Submissions.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    while(n--)
+    {
+        int x;
+        cin>>x;
+        int y=x*60;
+        cout<<y/30<<endl;
+    }
+	// your code goes here
+	return 0;
+}
