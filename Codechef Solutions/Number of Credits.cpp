e93ebaf0a08a8de7f00a8b5a22a441aa00400d5e
arsenic011 <116710792+arsenic011@users.noreply.github.com>
2022-10-26 13:21:18 +0530

    Create Number of Credits.cpp

---
+++ Codechef Solutions/Number of Credits.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,y,z;
+	    cin>>x>>y>>z;
+	    int a=x*4;
+	    int b=y*2;
+	    cout<<a+b<<endl;
+	}
+	return 0;
+}
+       
