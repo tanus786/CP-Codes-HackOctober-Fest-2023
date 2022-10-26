5cfab648cda48778212c870ee32e7cc4a52fdbd0
viratden <116731088+viratden@users.noreply.github.com>
2022-10-26 18:29:35 +0530

    Create Richie Rich.cpp

---
+++ Codechef Solutions/Richie Rich.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++)
+    {
+        int a,b,x;
+        cin>>a>>b>>x;
+        int z=(b-a)/x;
+        cout<<z<<endl;
+    }
+    return 0;
+}
