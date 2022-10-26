f5e6f4d8e2891f422be6fc57a7f6a6610f54ec17
arsenic011 <116710792+arsenic011@users.noreply.github.com>
2022-10-26 13:19:17 +0530

    Create Ticket Fine.cpp

---
+++ Codechef Solutions/Ticket Fine.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int n,a,b,c;
+    cin>>n;
+    while (n--)
+    {
+        cin>>a>>b>>c;
+        cout<<a*(b-c)<<endl;
+    }
+    return 0;
+}
+       
