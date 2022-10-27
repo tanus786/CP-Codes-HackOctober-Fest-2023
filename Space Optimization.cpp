dca9cb3996ddf1b8ec1957af8aaaa7055b18235d
wolv-28 <116797233+wolv-28@users.noreply.github.com>
2022-10-27 14:41:43 +0530

    Space Optimization.cpp

---
+++ Space Optimization.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+
+  int n=5;
+  
+  int prev2 = 0;
+  int prev = 1;
+  
+  for(int i=2; i<=n; i++){
+      int cur_i = prev2+ prev;
+      prev2 = prev;
+      prev= cur_i;
+  }
+  cout<<prev;
+  return 0;
+}
