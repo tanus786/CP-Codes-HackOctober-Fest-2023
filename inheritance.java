845091663d5f1ca834fab9f3b8c9b9b66b5dda51
parth-u <115478472+parth-u@users.noreply.github.com>
2022-10-27 18:09:55 +0530

    Create inheritance.java

---
+++ inheritance.java
@@ -0,0 +1,34 @@
+class Animal {
+
+  // field and method of the parent class
+  String name;
+  public void eat() {
+    System.out.println("I can eat");
+  }
+}
+
+// inherit from Animal
+class Dog extends Animal {
+
+  // new method in subclass
+  public void display() {
+    System.out.println("My name is " + name);
+  }
+}
+
+class Main {
+  public static void main(String[] args) {
+
+    // create an object of the subclass
+    Dog labrador = new Dog();
+
+    // access field of superclass
+    labrador.name = "Rohu";
+    labrador.display();
+
+    // call method of superclass
+    // using object of subclass
+    labrador.eat();
+
+  }
+}
