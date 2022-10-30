7b5fc0a58e9c0029404a04b95b006a3c96b50931
ankitkamdi <106524124+ankitkamdi@users.noreply.github.com>
2022-10-30 13:35:19 +0530

    Create Multitwofloat.c

---
+++ Multitwofloat.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+
+    double a, b, product;
+
+    printf("Enter two numbers: ");
+
+    scanf("%lf %lf", &a, &b);  
+
+       
+
+
+
+     product = a * b;
+
+   
+
+    printf("Product = %.2lf", product);
+
+    
+
+    return 0;
+
+}
+
