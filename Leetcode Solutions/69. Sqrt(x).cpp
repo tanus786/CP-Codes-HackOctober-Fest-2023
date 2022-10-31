6399b0f1ffb111bb0c049ff1f9a9d6f2b7278083
kashishbeotra <100989978+kashishbeotra@users.noreply.github.com>
2022-10-31 23:41:27 +0530

    add 69. Sqrt(x)
    added leetcode solution for 69. Sqrt(x)

---
+++ Leetcode Solutions/69. Sqrt(x).cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+     long long int binarySearch(int n) {
+        
+        int s = 0;
+        int e = n;
+        long long int mid = s + (e-s)/2;
+        
+        long long int ans = -1;
+        while(s<=e) {
+            
+            long long int square = mid*mid;
+            
+            if(square == n)
+                return mid;
+            
+            if(square < n ){
+                ans = mid;
+                s = mid+1;
+            }
+            else
+            {
+                e = mid - 1;
+            }
+            mid = s + (e-s)/2;
+        }
+        return ans;
+    }
+
+
+    int mySqrt(int x) {
+      return binarySearch(x);  
+    }
+};
