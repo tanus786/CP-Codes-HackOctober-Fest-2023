930ab842afee802d78f3f0ed5f90c56f64dc8597
Kundan Kumar <89348788+mindexpert7546@users.noreply.github.com>
2023-10-22 23:02:12 +0530

    Make the string great problem with full explanation
    I have added the important code for the make the string great in java with full explanation please review and merge my code with hacktoberfest-accepted lebal.....

---
+++ Stack/MakeStringGreat.java
@@ -0,0 +1,59 @@
+/**
+Given a string s of lower and upper case English letters.
+
+A good string is a string which doesn't have two adjacent characters s[i] and s[i + 1] where:
+
+0 <= i <= s.length - 2
+s[i] is a lower-case letter and s[i + 1] is the same letter but in upper-case or vice-versa.
+To make the string good, you can choose two adjacent characters that make the string bad and remove them. You can keep doing this until the string becomes good.
+
+Return the string after making it good. The answer is guaranteed to be unique under the given constraints.
+
+Notice that an empty string is also good.
+
+ 
+
+Example 1:
+
+Input: s = "leEeetcode"
+Output: "leetcode"
+Explanation: In the first step, either you choose i = 1 or i = 2, both will result "leEeetcode" to be reduced to "leetcode".
+Example 2:
+
+Input: s = "abBAcC"
+Output: ""
+Explanation: We have many possible scenarios, and all lead to the same answer. For example:
+"abBAcC" --> "aAcC" --> "cC" --> ""
+"abBAcC" --> "abBA" --> "aA" --> ""
+Example 3:
+
+Input: s = "s"
+Output: "s"
+ 
+
+Constraints:
+
+1 <= s.length <= 100
+s contains only lower and upper case English letters.
+*/
+
+class Solution {
+    public String makeGood(String s) {
+        Stack<Character> ch = new Stack<>();
+        for(int i=0; i<s.length(); i++){
+            if(!ch.isEmpty() && Math.abs(ch.peek() - s.charAt(i))==32){
+                ch.pop();
+            }
+            else{
+                ch.push(s.charAt(i));
+            }
+        }
+
+        StringBuilder sb = new StringBuilder("");
+        while(!ch.isEmpty()){
+            sb.append(String.valueOf(ch.pop()));
+        }
+
+        return sb.reverse().toString();
+    }
+}
